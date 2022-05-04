f4c98a46ddef5e89259146120a2622a98635613a
Emir Mendez <mendez.a.emir@gmail.com>
2022-05-04 16:33:44 -0700

    added files for bash script

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd my_website / 
+
+cat .templates/top.html  .content/contact.html  .templates/bottom.html > contactpage.html 
+
+cat .templates/top.html content/index.html templates/bottom.html >  indexpage.html 
+
+cat .templates/top.html content/project.html templates/bottom.html > projectpage.html 
