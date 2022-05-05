113ccaffbd2f9b13cf5c59ae131ff2b9d4a18c13
Emir Mendez <mendez.a.emir@gmail.com>
2022-05-04 18:20:53 -0700

    more changes on image folder for bash.sh to run proper

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd Resume/ 
-cat templates/top.html  content/contact.html  templates/bottom.html > docs/contact.html 
+cd User/mars/Resume
+cat templates/top.html  content/contact.html templates/bottom.html > docs/contact.html 
 cat templates/top.html content/index.html templates/bottom.html >  docs/index.html 
 cat templates/top.html content/project.html templates/bottom.html > docs/project.html 
